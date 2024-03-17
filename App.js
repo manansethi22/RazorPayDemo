58bc2ff83489514172d2794492ed43992545f73b
manansethi22 <manansethi2002@gmail.com>
2024-03-18 03:39:18 +0530

    1

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import * as SystemUI from 'expo-system-ui';
+import { SafeAreaView, FlatList, TextInput, Button } from "react-native";
+import RazorpayCheckout from "react-native-razorpay";
+let key = 'rzp_test_HKRrXYXoOWHaGa'
+const App = () => {
+  const amount = 100;
+  const handlePayment = () => {
+    var options = {
+      description: "Credits towards consultation",
+      image: "https://i.imgur.com/3g7nmJC.jpg",
+      currency: "INR",
+      key: key,
+      amount: amount*100,
+      name: "Acme Corp",
+      order_id: '', 
+      prefill: {
+        email: "xyz@gmail.com",
+        contact: "9191919191",
+        name: "Gaurav Kumar",
+      },
+      theme: { color: "#53a20e" },
+    };
+    RazorpayCheckout.open(options)
+      .then((data) => {
+        // handle success
+        alert(`Success: ${data.razorpay_payment_id}`);
+      })
+      .catch((error) => {
+        // handle failure
+        alert(`Error: ${error.code} | ${error.description}`);
+      });
+  };
 
-export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ marginTop: 80, padding: 20 }}>
+      <Button
+        style={{ marginTop: 120 }}
+        title="Pay Now"
+        onPress={handlePayment}
+      />
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
